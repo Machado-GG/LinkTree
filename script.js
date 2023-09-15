e9689ac3d9e1a9d47399f75737dc45d27a8f5f40
Gabriel Machado <goncalvesgm96@gmail.com>
2023-09-15 15:35:07 -0300

    ":tada: initial commit"

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+ 
+  // pegar a img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem  
+  if(html.classList.contains("light")) {
+
+    img.setAttribute("src", "./assets/avatarbill-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatarbill.png")
+  }
+
+}
